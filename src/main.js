d69cf67744ff5b126611ee366026591a7b6a477c
ScheerleJo <scheerle.josia@gmail.com>
2023-11-15 17:35:19 +0100

    Update GUI styling and minor functionality improvements

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ const { invoke } = window.__TAURI__.tauri;
 let greetInputEl;
 let greetMsgEl;
 
+setListingStatus();
+
+
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
@@ -18,16 +21,46 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 
+document.getElementById('input-year').value = new Date().getFullYear()
+
+function showElement(elementID) {
+  let element = document.getElementById(elementID);
+  element.style.display = "block";
+}
+function hideElement(elementID) {
+  let element = document.getElementById(elementID);
+  element.style.display = "none";
+}
+
+function toggleCaret(elementID) {
+  let donationTbl = document.getElementById('donation-' + elementID.split('-')[1]);
+  let anchorCaret = document.getElementById(elementID).firstChild;
+  let anchorContentRight = '<i class="fa-solid fa-caret-right"></i>'
+  if(anchorCaret.innerHTML == anchorContentRight) {
+    donationTbl.style.display = "block";
+    anchorCaret.innerHTML = '<i class="fa-solid fa-caret-down"></i>';
+  } else {
+    donationTbl.style.display = "none";
+    anchorCaret.innerHTML = '<i class="fa-solid fa-caret-right"></i>';
+  }
+}
 
 
-document.getElementById('input-your').value = new Date().toUTCString()
-
+function setStartStatus() {
+  showElement('start-container');
+  hideElement('listing-container');
+}
+function setListingStatus(state) {
+  showElement('listing-container');
+  hideElement('start-container');
+  showTable(state);
+}
 
 function showTable(state) {
   /*
     State 0: click on Tab 'open'
     State 1: click on Tab 'check'
-    State 0: click on Tab 'done'
+    State 2: click on Tab 'done'
     Default: send alert with warning and display all Donations no matter what state
 
   */
