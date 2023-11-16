83c60802009bd427662b8b007b5d8a2627389db1
Josia <scheerle.josia@gmail.com>
2023-11-16 16:26:55 +0100

    Add Table AutoCreate Functionality

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import { appWindow } from '@tauri-apps/api/window';
+
 const { invoke } = window.__TAURI__.tauri;
 
 let greetInputEl;
@@ -19,6 +21,20 @@ window.addEventListener("DOMContentLoaded", () => {
     greet();
   });
 });
+
+
+
+document
+  .getElementById('titlebar-minimize')
+  .addEventListener('click', () => appWindow.minimize())
+document
+  .getElementById('titlebar-maximize')
+  .addEventListener('click', () => appWindow.toggleMaximize())
+document
+  .getElementById('titlebar-close')
+  .addEventListener('click', () => appWindow.close())
+
+
 
 
 document.getElementById('input-year').value = new Date().getFullYear()
@@ -45,7 +61,54 @@ function toggleCaret(elementID) {
   }
 }
 
-
+function showAlert(){
+  let userInfo = [
+    // [
+    //   customernumber,
+    //   title,
+    //   firstName,
+    //   lastName,
+    //   streetNumber,
+    //   postalCodeCity,
+    //   donationSum,
+    //   [
+    //     ['01.01.2023', 'Spende', 'Verzicht auf Spendenbescheinigung', '250€'],
+    //     ['01.02.2023', 'Spende', 'Verzicht auf Rückerstattung', '250€'],
+    //     ['01.03.2023', 'Spende', 'Verzicht', '250€']],
+    //   state
+    // ],
+    [
+      '1',
+      'title',
+      'firstName',
+      'lastName',
+      'streetNumber',
+      '74343 Hohenhaslach',
+      'donationSum',
+      [
+        ['01.01.2023', 'Spende', 'Verzicht auf Spendenbescheinigung', '250€'],
+        ['01.02.2023', 'Spende', 'Verzicht auf Rückerstattung', '250€'],
+        ['01.03.2023', 'Spende', 'Verzicht', '250€']], 0
+    ],
+    [
+      '2',
+      'title',
+      'firstName',
+      'lastName',
+      'streetNumber',
+      'postalCodeCity',
+      'donationSum',
+      [
+        ['01.01.2023', 'Spende', 'Verzicht auf Spendenbescheinigung', '250€'],
+        ['01.02.2023', 'Spende', 'Verzicht auf Rückerstattung', '250€'],
+        ['01.03.2023', 'Spende', 'Verzicht', '250€']], 0
+  ],]
+  // let donationInfo = [[
+    // ['01.01.2023', 'Spende', 'Verzicht auf Spendenbescheinigung', '250€'],
+    // ['01.02.2023', 'Spende', 'Verzicht auf Rückerstattung', '250€'],
+    // ['01.03.2023', 'Spende', 'Verzicht', '250€']]];
+  createUserTable(userInfo, 1);
+}
 function setStartStatus() {
   showElement('start-container');
   hideElement('listing-container');
@@ -61,27 +124,112 @@ function showTable(state) {
     State 0: click on Tab 'open'
     State 1: click on Tab 'check'
     State 2: click on Tab 'done'
-    Default: send alert with warning and display all Donations no matter what state
-
   */
-  switch (state){
-    case 0:
+  let data = getData()   //TODO: Code the process of getting all Data to display in the table
+  createUserTable(data, state);
+}
 
+function createUserTable(userInfo, state) {
 
-    break;
-    case 1:
+  let tblBody = document.getElementById('autocreate-table-body');
 
+  if(tblBody.firstChild){
+    alert('da is noch wat')
+  }
+  else {
+    alert('alles weg')
+  }
 
+  for (let userIndex = 0; userIndex < userInfo.length; userIndex++) {
+    let tr = document.createElement('tr');
+    tr.setAttribute('class', 'donator-autocreate-tr');
+    tr.setAttribute('id', `donator-${userIndex}`);
+    for (let j = 0; j < 9; j++) {
+      let td = document.createElement('td');
+      td.setAttribute('class', `donator-element-${j}`);
+      if (j == 0) {
+        td.setAttribute('class', 'align-center caret');
+        td.setAttribute('id', `caret-${userIndex}`);
+        let a = document.createElement('a')
+          .setAttribute('href', '#')
+          .setAttribute('onclick', `toggleCaret('caret-${userIndex}')`)
+          .appendChild(document.createElement('i').setAttribute('class', 'fa-solid fa-caret-down'));
+        
+        td.appendChild(a);
+      } else if (j == 1) {
+        td.setAttribute('class', 'align-center');
+        let input = document.createElement('input')
+          .setAttribute('class', 'checkbox-done')
+          .setAttribute('type', 'checkbox');
 
-    break;
-    case 2:
-
-
-    break;
-    default:
-
-
-    break;
+        td.appendChild(input);
+      } else {
+        let span = document.createElement('span');
+        span.appendChild(document.createTextNode(userInfo[userIndex][j - 2]));
+        td.appendChild(span);
+      }
+      tr.appendChild(td);
+    }
+    tblBody.appendChild(tr);
+    let donationInfo = userInfo[userIndex][7];
+    createDonationTable(donationInfo, userIndex);
   }
 }
 
+/**
+ * Create Table with all Donations Listed in SevDesk to the User above
+ * @param  {Array<Array<string>} donationInfo
+ * 2 Dimensional Array, with needed info for a Donation per row of Array
+ * @param  {number} userIndex
+ * Number of associated User in Table
+ */
+function createDonationTable(donationInfo, userIndex) {
+  // donationInfo Sample = [[date, type, waive, sum], [date, type, waive, sum], [...], ...]
+  if(document.getElementById(`donation-${userIndex}`) != undefined) {
+    return false;
+  }
+  let tblBody = document.getElementById('autocreate-table-body');
+  let outerTr = document.createElement('tr');
+  let outerTd = document.createElement('td');
+  outerTr.setAttribute('class', 'donation-autocreate-tr');
+  outerTr.appendChild(document.createElement('td'));
+  
+  let innerTbl = document.createElement('table');
+  innerTbl.setAttribute('id', `donation-${userIndex}`);
+  innerTbl.setAttribute('class', 'donation-table');
+  let innerTrHead = document.createElement('tr');
+  
+  let innerHeadInfo = ['Datum', 'Art', 'Verzicht', 'Betrag'];
+  for (let i = 0; i < innerHeadInfo.length; i++) {
+    let innerTh = document.createElement('th');
+    innerTh.appendChild(document.createTextNode(innerHeadInfo[i]));
+    innerTrHead.appendChild(innerTh);
+  }
+  innerTbl.appendChild(innerTrHead);
+  for (let i = 0; i < donationInfo.length; i++) {
+    const element = donationInfo[i];
+    let innerTr = document.createElement('tr');
+    for (let j = 0; j < 4; j++) {
+      let innerTd = document.createElement('td');
+      innerTd.appendChild(document.createTextNode(element[j]));
+      innerTr.appendChild(innerTd);
+    }
+    innerTbl.appendChild(innerTr);
+  }
+  outerTd.setAttribute('colspan', 8);
+  outerTd.appendChild(innerTbl);
+  outerTr.appendChild(outerTd);
+  tblBody.appendChild(outerTr);
+
+  toggleCaret(`caret-${userIndex}`);
+  return true;
+}
+
+function clearAllOnStartUp(){
+  let tblBody = document.getElementById('autocreate-table-body');
+  while (tblBody.firstChild) {
+    tblBody.removeChild(tblBody.firstChild);
+  }
+}
+
+clearAllOnStartUp();
